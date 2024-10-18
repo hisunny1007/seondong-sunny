7890227f954ac08ea073ab27657e4376497059bf
hisunny1007 <hisunny1007@gmail.com>
2024-10-18 17:59:20 +0900

    241018 자바스크립트 실습 숫자 크기 비교

---
+++ javascript/1018/과제06_4.js
@@ -0,0 +1,19 @@
+//4. 숫자 크기 비교2
+
+let number_1 = 5;
+
+console.log(number_1);
+
+if (number_1 < 10) {
+  console.log('number_1은 10보다 작다');
+}
+
+if (number_1 > 10) {
+  console.log('number_1은 10보다 크다');
+}
+
+if (number_1 < 10) {
+  console.log('number_1은 10보다 작다');
+} else if (number_1 > 10) {
+  console.log('number_1은 10보다 크다');
+}
