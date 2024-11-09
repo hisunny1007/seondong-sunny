0508f2e9f530f08b4bd05f9d5ec26731f095bb1e
hisunny1007 <hisunny1007@gmail.com>
2024-11-09 21:38:20 +0900

    241109 알고리즘 문자열 문제

---
+++ python/a.py
@@ -0,0 +1,10 @@
+
+# word = 'Sprout'
+# print(word[2])
+
+S = (input())
+i = int(input())
+
+print(S[i-1])
+
+# for _ in range(i):
---
+++ 알고리즘/백준/문자와문자열.py
@@ -0,0 +1,12 @@
+# 27866번 문자와 문자열
+# https://www.acmicpc.net/problem/27866
+
+# Q. 단어 S와 정수 i가 주어졌을 때, S의 i번째 글자 출력
+
+# word = 'Sprout'
+# print(word[2])
+
+S = input()
+i = int(input())
+
+print(S[i-1])
