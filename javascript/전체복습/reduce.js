977d9053e4fdbc3a5b422b2c34152d5eb8b2ebd4
hisunny1007 <hisunny1007@gmail.com>
2024-12-09 23:55:06 +0900

    javascript reduce

---
+++ javascript/전체복습/reduce.js
@@ -0,0 +1,17 @@
+// reduce 함수
+// 배열의 원소를 순차적으로 누적하여 하나의 값으로 반환
+
+const numbers = [1, 2, 3, 4];
+
+// accumulator : 누적값
+// currentValue : 배열 원소 값
+const sum = numbers.reduce((accumulator, currentValue) => {
+  console.log(accumulator, currentValue);
+  // 0 1
+  // 1 2
+  // 3 3
+  // 6 4
+  return accumulator + currentValue;
+}, 0);
+
+console.log(sum); // 10
